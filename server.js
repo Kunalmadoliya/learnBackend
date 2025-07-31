3c00086114a3a668ba1ced5271ab31121b646c52
Kunal Madoliya <madoliyakunal2005@gmail.com>
2025-07-31 22:42:00 +0530

    31-07-25

--- server.js
+++ server.js
@@ -1,33 +1,63 @@
 const express = require("express");
-const connectToDb = require("./src/db/db")
+const connectToDb = require("./src/db/db");
+const userModel = require("./src/models/user.model");
 
-const app = express();//creating server
-connectToDb()//connecting DB
+const app = express();
+connectToDb();
 
+app.use(express.json());
 
-
-app.use(express.json()); //middleware
-
-
-//routes
-let userData = [];
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-
-app.post("/profile", (req, res) => {
+app.post("/signup", async (req, res) => {
   const {user, password} = req.body;
   console.log(req.body);
 
+  await userModel.create({
+    user,
+    password,
+  });
+
   res.json({
-    message: `User Data send Successfully`,
+    message: "User SignedIn ",
   });
 });
 
+app.get("/signup", async (req, res) => {
+  const users = await userModel.find();
 
-//server-listening
+  res.json({
+    message: "User fetch Sucess",
+    users,
+  });
+});
+
+app.patch("/signup/:id", async (req, res) => {
+  const userId = req.params.id;
+  const {password} = req.body;
+
+  await userModel.findByIdAndUpdate(
+    {
+      _id: userId,
+    },
+    {
+      password: password,
+    }
+  );
+
+  res.json({
+    message : "User Updated!"
+  })
+});
+
+app.delete("/signup/:id", async (req, res) => {
+  const userId = req.params.id;
+  await userModel.findOneAndDelete({
+    _id: userId,
+  });
+  res.json({
+    message: "User Deleted Success",
+  });
+});
+
 app.listen(3000, () => {
-  console.log("Server started at Port 3000");
+  console.log("Server running on port 3000");
 });
--- src/db/db.js
+++ src/db/db.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
 
 function connectToDb() {
-  mongoose.connect(
-    "mongodb+srv://kunal:LP498G3ZOjzETSTE@cohort.mkjliqy.mongodb.net/cohort"
-  ).then(()=>{
-    console.log("Connected to DB");
-    
-  })
+  mongoose
+    .connect(
+      "mongodb+srv://kunal:LP498G3ZOjzETSTE@cohort.mkjliqy.mongodb.net/cohort"
+    )
+    .then(() =>{console.log("Database connected ")});
 }
 
 
