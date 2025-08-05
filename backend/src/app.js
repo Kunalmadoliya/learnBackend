ffc0b9bad73b14ad40d6e8ceebd7564facb850c0
Kunal Madoliya <madoliyakunal2005@gmail.com>
2025-08-05 18:53:13 +0530

    5-08-25

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ const songRoutes = require("./routes/song.routes");
 
 const app = express();
 app.use(express.json());
+
 app.use("/", songRoutes);
 
 module.exports = app;
--- backend/src/routes/song.routes.js
+++ backend/src/routes/song.routes.js
@@ -1,14 +1,20 @@
 const express = require("express");
-const multer = require("multer")
+const multer = require("multer");
 const router = express.Router();
+const uploadFile = require("../services/storage.service");
+const upload = multer({storage: multer.memoryStorage()});
 
+router.post("/songs", upload.single("audio"), async (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
 
+  const fileData = await uploadFile(req.file);
+  console.log(fileData);
 
-const upload = multer({storage : multer.memoryStorage()})
-
-
-
-
-router.post("/songs",  (req, res) => {});
+  res.status(201).json({
+    message: "Songs created successsfully",
+    songs: req.body,
+  });
+});
 
 module.exports = router;
